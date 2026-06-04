df95097ad2c58a2d869e2d03ffb0cf382fa75cb4
r3ngem <paclob.m@northeastern.edu>
2026-06-04 18:40:10 +0200

    diagnostic survey ui

--- app/src/pages/02_Diagnostic_Survey.py
+++ app/src/pages/02_Diagnostic_Survey.py
@@ -10,93 +10,51 @@ st.set_page_config(layout='wide')
 
 SideBarLinks()
 
-# set up the page
-st.markdown("# Mapping Demo")
-st.sidebar.header("Mapping Demo")
-st.write(
-    """This Mapping Demo is from the Streamlit Documentation. It shows how to use
-[`st.pydeck_chart`](https://docs.streamlit.io/library/api-reference/charts/st.pydeck_chart)
-to display geospatial data."""
+st.header('Welcome to the Diagnostic Survey')
+st.write('### Take the survey to determine your trust in EU institutions')
+
+st.number_input(
+    label='Age:',
+        value = 22,
+        key="age"
 )
 
+st.selectbox(
+    label="Level of Education:",
+    options=["Middle School", "High School", "Bachelor's", "Master's", "Doctorate"],
+    key="level_of_edu",
+    index=None,
+    placeholder="Select Education"
+)
 
-@st.cache_data
-def from_data_file(filename):
-    url = (
-        "http://raw.githubusercontent.com/streamlit/"
-        "example-data/master/hello/v1/%s" % filename
-    )
-    return pd.read_json(url)
+st.selectbox(
+    label="Gender:",
+    options=["Male", "Female", "Non-Binary", "Prefer Not to Say"],
+    key="gender",
+    index=None,
+    placeholder="Select Gender"
+)
 
+st.write('### How would you rate your political interest?')
+st.radio(
+        label="",
+        options= ["1 - Very Low", "2 - Low", "3 - Neutral", "4 - High", "5 - Very High"],
+        key="pol_interest",
+        label_visibility="collapsed"
+)
 
-try:
-    ALL_LAYERS = {
-        "Bike Rentals": pdk.Layer(
-            "HexagonLayer",
-            data=from_data_file("bike_rental_stats.json"),
-            get_position=["lon", "lat"],
-            radius=200,
-            elevation_scale=4,
-            elevation_range=[0, 1000],
-            extruded=True,
-        ),
-        "Bart Stop Exits": pdk.Layer(
-            "ScatterplotLayer",
-            data=from_data_file("bart_stop_stats.json"),
-            get_position=["lon", "lat"],
-            get_color=[200, 30, 0, 160],
-            get_radius="[exits]",
-            radius_scale=0.05,
-        ),
-        "Bart Stop Names": pdk.Layer(
-            "TextLayer",
-            data=from_data_file("bart_stop_stats.json"),
-            get_position=["lon", "lat"],
-            get_text="name",
-            get_color=[0, 0, 0, 200],
-            get_size=15,
-            get_alignment_baseline="'bottom'",
-        ),
-        "Outbound Flow": pdk.Layer(
-            "ArcLayer",
-            data=from_data_file("bart_path_stats.json"),
-            get_source_position=["lon", "lat"],
-            get_target_position=["lon2", "lat2"],
-            get_source_color=[200, 30, 0, 160],
-            get_target_color=[200, 30, 0, 160],
-            auto_highlight=True,
-            width_scale=0.0001,
-            get_width="outbound",
-            width_min_pixels=3,
-            width_max_pixels=30,
-        ),
-    }
-    st.sidebar.markdown("### Map Layers")
-    selected_layers = [
-        layer
-        for layer_name, layer in ALL_LAYERS.items()
-        if st.sidebar.checkbox(layer_name, True)
-    ]
-    if selected_layers:
-        st.pydeck_chart(
-            pdk.Deck(
-                map_style="mapbox://styles/mapbox/light-v9",
-                initial_view_state={
-                    "latitude": 37.76,
-                    "longitude": -122.4,
-                    "zoom": 11,
-                    "pitch": 50,
-                },
-                layers=selected_layers,
-            )
-        )
-    else:
-        st.error("Please choose at least one layer above.")
-except URLError as e:
-    st.error(
-        """
-        **This demo requires internet access.**
-        Connection error: %s
-    """
-        % e.reason
-    )
+st.write('### How would you rate your trust in your national parliament?')
+st.radio(
+        label="",
+        options= ["1 - Very Low", "2 - Low", "3 - Neutral", "4 - High", "5 - Very High"],
+        key="nat_parliament_trust",
+        label_visibility="collapsed"
+)
+
+st.write('### How would you rate your trust in politicians?')
+st.radio(
+        label="",
+        options= ["1 - Very Low", "2 - Low", "3 - Neutral", "4 - High", "5 - Very High"],
+        key="politician_trust",
+        label_visibility="collapsed"
+)
